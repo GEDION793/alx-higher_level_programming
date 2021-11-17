0c5825439c6ceef050868ce6cca8e5542c6f982d
GEDION793 <84683857+GEDION793@users.noreply.github.com>
2021-11-17 17:59:12 +0000

    fix

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- script that inserts a new row in the table first_table
-INSERT INTO first_table (id,name)
-VALUES ('89', 'Holberton School');
+--Insert into
+INSERT INTO hbtn_0c_0.first_table (id, `name`) VALUES("89", "Best School");
